787f0fbdec86b514030f76e5050c87c8bdf95291
nurazom050 <nurazom048@gmail.com>
2023-01-31 10:13:10 +0600

    m vc

--- sarver.js
+++ sarver.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-var jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const Account = require('./models/Account')
+const rutin_route = require('./routes/rutin_routes');
+const auth_route = require('./routes/auth_route');
+const class_route = require('./routes/class_route');
+
+
+
 
 //..... Connection
 mongoose.connect('mongodb+srv://nurapp:rr1234@cluster0.wwfxxwu.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('Connected!'));
-
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 
 
@@ -19,6 +21,11 @@ app.use(bodyParser.json())
 
 
 
+// ------- Routes ------------------------//
+
+app.use("/auth",auth_route); //.. auth_route
+app.use("/routine",rutin_route );//.. rutin_route
+app.use("/class",class_route);//.. class_route
 
 
 
@@ -26,67 +33,6 @@ app.use(bodyParser.json())
 
 
 
-// login 
-
-app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      // Find user by username
-      const user = await Account.find({ username });
-      if (!user) return res.status(400).json({ message: "Invalid credentials" });
-  
-      // Compare passwords
-     
-      if (password != user.password) return res.status(400).json({ message: "Invalid credentials" });
-  
-      // Create a JWT token
-      const token = jwt.sign({ id: user._id }, "secret", { expiresIn: "1d" });
-  
-      // Send response with token
-      res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error logging in" });
-    }
-  });
-  
-
-
-
-
-
-
-
-
-//..... creat 
-
-  app.post("/creat",async (req, res)=> {
-    const { name, username, password } = req.body
-    const existingUser = await Account.findOne({ username });
-   
-   
-    try {
-      if (existingUser) return res.status(400).json({ message: "Username already exists" });
-  
-      // Create a new account
-      const account = new Account({ name, username, password });
-      const created = await account.save();
-      // Send response
-      res.status(200).json({ message: "Account created successfully", created });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error creating account" });
-    }
-   
-
-
-
-  
-  })
-
-
-
-  app.listen(3000, function(){
-    console.log(" ****server started ********");
+app.listen(3000, function(){
+  console.log(" ****server started ********");
 });
